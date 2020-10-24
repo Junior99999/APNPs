89946388d6fecc2cdc980b0ac456c74fa6434861
Junior99999 <juniormachdo11@gmail.com>
2020-10-24 20:11:27 -0300

    Adição da rota Mensagem

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res){
     res.send('Olá josé')
 });
 
+app.get("/mensagem", function(req,res){
+    res.send("Essa mensagem é automatica!")
+});
+
 app.listen(3000,function(){
     console.log("Conexão inicializada")
 });
